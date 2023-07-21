fd7f42f358d14079ab8ed6497441423b2aa1dfa4
samarasena-iach <C5lus9tering$>
2023-07-21 15:10:29 +0530

    Added the comment for YT Tutorial Reference

--- src/main/java/com/samiach/kafkadockerdemo/KafkaDockerDemoApplication.java
+++ src/main/java/com/samiach/kafkadockerdemo/KafkaDockerDemoApplication.java
@@ -3,6 +3,7 @@ package com.samiach.kafkadockerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// YT Tutorial : Spring Boot with Kafka & Docker _ What is Kafka _ Kafka tutorial with a step by step example
 @SpringBootApplication
 public class KafkaDockerDemoApplication {
 
